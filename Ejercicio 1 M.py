83cda0446c43a32125fbf102336ca1eccec9eeac
YvCuevas1983 <159745067+YvCuevas1983@users.noreply.github.com>
2024-02-25 22:02:36 -0500

    Add files via upload

---
+++ Ejercicio 1 M.py
@@ -0,0 +1,18 @@
+def buscar_valor(matriz, valor):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == valor:
+                return f"El valor {valor} se encontró en la posición ({i}, {j})"
+    return "El valor no se encontró en la matriz"
+
+
+# Matriz de ejemplo 3x3
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matriz)
+
+valor_a_buscar = 8
+print(buscar_valor(matriz, valor_a_buscar))
---
+++ Ejercicio 2 M.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def ordenar_fila_matriz(matrix, fila):
+    matrix[fila] = np.sort(matrix[fila])
+    return matrix
+
+# Crear una matriz de ejemplo de 3x3
+matriz = np.array([[9, 2, 5], [7, 1, 8], [4, 6, 3]])
+
+# Imprimir la matriz original
+print("Matriz original:")
+print(matriz)
+
+# Ordenar la segunda fila de la matriz
+matriz_ordenada = ordenar_fila_matriz(matriz, 1)
+
+# Imprimir la matriz con la fila ordenada
+print("\nMatriz con la segunda fila ordenada:")
+print(matriz_ordenada)
